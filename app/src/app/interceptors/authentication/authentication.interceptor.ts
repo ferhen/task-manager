6ab67acfc7b1588899021c579d250bec4981ddce
Fernando Eckstein <fernando_eckstein@hotmail.com>
2020-12-06 00:46:05 -0300

    feat: error messages

--- app/src/app/interceptors/authentication/authentication.interceptor.ts
+++ app/src/app/interceptors/authentication/authentication.interceptor.ts
@@ -9,10 +9,14 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
-    constructor(private readonly router: Router) { }
+    constructor(
+        private readonly router: Router,
+        private readonly snackBar: MatSnackBar
+    ) { }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         request = request.clone({
@@ -30,6 +34,17 @@ export class AuthenticationInterceptor implements HttpInterceptor {
             catchError((error: HttpErrorResponse) => {
                 if (error.status === 401) {
                     this.router.navigateByUrl('/');
+                } else {
+                    let errorMessage = '';
+                    if (error.status === 0) {
+                        errorMessage = 'Could not connect to server';
+                    } else {
+                        errorMessage = error.message;
+                    }
+                    this.snackBar.open(errorMessage, 'Close', {
+                        duration: 10000,
+                        panelClass: ['mat-warn']
+                    });
                 }
                 return throwError(error);
             })
